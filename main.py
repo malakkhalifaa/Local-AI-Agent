b1a4d8243bf4aca00ab4039c13db860238cb943c
malakkhalifa <malakkhalifa10@gmail.com>
2026-03-12 03:16:02 -0400

    done

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
-
-#These framwork that makes it easy to use ollama with langchain
-#Ollama should be running in the background for this to work
-#This will all run locally on your machine, no data is sent to the cloud
+from vector import retriever
 
 model = OllamaLLM(model="llama3.2")
 
-#What we want to ask the model
-template="""
+template = """
 You are an exeprt in answering questions about a pizza restaurant
 
 Here are some relevant reviews: {reviews}
 
 Here is the question to answer: {question}
 """
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
 while True:
     print("\n\n-------------------------------")
     question = input("Ask your question (q to quit): ")
--- vector.py
+++ vector.py
@@ -1,42 +1,37 @@
-#logic for vector database, uses chromadb, which is a local vector database
-#that can be used to store and retrieve vectors
-#take entire csv file and convert it to a vector database, 
-# then use that vector database to answer questions
-from langchain_ollM import OllamaLLM
-from langchain_chromadb import ChromaDB
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
 from langchain_core.documents import Document
 import os
 import pandas as pd
 
-#load csv file
-df=pd.read_csv("realife_reviews.csv")
-#bring embeddings for each review
-embeddings=OllamaEmbeddings(model="mxbai-embeddings-large")
-#store our vectors in chromadb
+df = pd.read_csv("realistic_restaurant_reviews.csv")
+embeddings = OllamaEmbeddings(model="mxbai-embed-large")
 
-
-db_location="./chrome_landchain_db"
-add_documents=not os.path.exists(db_location) #check if the database already exists, if it does we don't need to add documents again
-#if exists means vectors are already stored, if not we need to add them
-
+db_location = "./chrome_langchain_db"
+add_documents = not os.path.exists(db_location)
 
 if add_documents:
-    documents=[]
-    ids=[]
-
-    #iterate through each review and create a document for it
+    documents = []
+    ids = []
+    
     for i, row in df.iterrows():
-        document=Document(page_content=row["Title"]+ " " + row["Review"] #page content is what we re vectorizing, we can use the title and review text for this
-        metadata={"rating": row["Rating"], "date": row["Date"]} 
-        #metadata is additional information we want to store about the document, we can use the rating and date for this but wont be used for retrieval in this example
-       id=str(i) #id is a unique identifier for the document, we can just use the index of the review in the dataframe for this
+        document = Document(
+            page_content=row["Title"] + " " + row["Review"],
+            metadata={"rating": row["Rating"], "date": row["Date"]},
+            id=str(i)
         )
-        ids.append(str(i)) #need to convert to string because chromadb expects string ids
-        documents.append(document) #add the document to our list of documents
+        ids.append(str(i))
+        documents.append(document)
         
-vector_store = Chroma(collection_name="restaurant_reviews", 
-                      persist_directory=db_location, 
-                      embedding_function=embeddings
-                      ) #store persistently in the specified directory, rather than in memory
+vector_store = Chroma(
+    collection_name="restaurant_reviews",
+    persist_directory=db_location,
+    embedding_function=embeddings
+)
 
-if add_documents
+if add_documents:
+    vector_store.add_documents(documents=documents, ids=ids)
+    
+retriever = vector_store.as_retriever(
+    search_kwargs={"k": 5}
+)
