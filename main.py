8ec272f657dc44bb63a40a99ae9f0d3d9e7213e8
malakkhalifa <malakkhalifa10@gmail.com>
2026-03-12 02:37:56 -0400

    done

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from langchain_ollama.llms import OllamaLLM
-from lanchain_core.prompts import PromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 
 #These framwork that makes it easy to use ollama with langchain
 #Ollama should be running in the background for this to work
 #This will all run locally on your machine, no data is sent to the cloud
 
-model = OllamaLLM(model="llama3")
+model = OllamaLLM(model="llama3.2")
 
 #What we want to ask the model
 template="""
